111f7201d59bd6d493471694830fc88ccf2411ea
rumanikafle99 <mishti4400@gmail.com>
2025-02-18 18:55:40 -0500

    final draft

--- barChart.js
+++ barChart.js
@@ -22,7 +22,7 @@ let summary = {
         "Fine particles (PM 2.5)" : {sum: 0, count:0},
         "Nitrogen dioxide (NO2)" : {sum: 0, count:0}
     },
-    "StatenIsland": {
+    "Staten Island": {
         "Fine particles (PM 2.5)" : {sum: 0, count:0},
         "Nitrogen dioxide (NO2)" : {sum: 0, count:0}
     }
@@ -61,25 +61,82 @@ let boroughNames = Object.keys(pollutionAverage); //puts keys --- borough names 
 console.log(boroughNames);
 
 let pollutantData = Object.values(pollutionAverage); //puts values of each boroughs -- pollutant info ---inside a list 
-console.log(pollutantData);
+console.log(pollutantData); 
+
+//for each borough in boroughNames array... look at pollutionAverage with boro as the key
+let NitrogenOxideData = boroughNames.map(boro => pollutionAverage[boro]["Nitrogen dioxide (NO2)"]);
+console.log(NitrogenOxideData); 
+
+let fineParticleData = boroughNames.map(boro2 => pollutionAverage[boro2]["Fine particles (PM 2.5)"]);
+console.log(fineParticleData); 
+
+
 
 /*Brooklyn*/ 
-let BrooklynValues = Object.values(pollutionAverage.Brooklyn);
-console.log(BrooklynValues);
+//let BrooklynValues = Object.values(pollutionAverage.Brooklyn);
+//console.log(BrooklynValues);
 
 /*Queens*/ 
-let QueensValues = Object.values(pollutionAverage.Queens);
+//let QueensValues = Object.values(pollutionAverage.Queens);
 
 /*Bronx*/
-let BronxValues = Object.values(pollutionAverage.Bronx);
+//let BronxValues = Object.values(pollutionAverage.Bronx);
 
 /*Manhattan*/
-let ManhattanValues = Object.values(pollutionAverage.Manhattan);
+//let ManhattanValues = Object.values(pollutionAverage.Manhattan);
 
 /*StatenIsland*/
-let StatenIslandValues = Object.values(pollutionAverage.StatenIsland);
+//let StatenIslandValues = Object.values(pollutionAverage.StatenIsland);
+
+let NitrogenOxideDataTrace = {
+    x: boroughNames,
+    y: NitrogenOxideData,
+    text: "NO2",
+    name: "Nitrogen dioxide (NO2)",
+    type: "bar",
+    marker: {
+        color: "green"
+    }
+};
 
 
+let FineParticlesDataTrace = {
+    x: boroughNames,
+    y: fineParticleData,
+    text: "PM 2.5",
+    name: "Fine particles (PM 2.5)",
+    type: "bar",
+    marker: {
+        color: "light yellow"
+    }
+};
 
+let layout={
+    title: "Nitrogen Dioxide vs. Fine Particles",
+    barmode: "group",
+    xaxis: {
+        title: "NYC Boroughs", 
+        tickfont: { size: 15, color: "black" },
+        titlefont:{ size: 18, color: "black" },
+    },
+    yaxis: {
+        title: "Average Pollutant Level",
+        tickfont: { size: 15, color: "black" },
+        titlefont:{ size: 18, color: "black" }
+    },
+    font: { family: "Tahoma, sans-serif" },
+    margin: { 
+        l:100,
+        r:50,
+        b:50,
+        t: 50, 
+        pad: 10
+    }
+
+};
+
+let dataTrace = [NitrogenOxideDataTrace, FineParticlesDataTrace];
+
+Plotly.newPlot("BarChartPlot", dataTrace,layout);
 
 
